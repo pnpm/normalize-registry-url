6b8ffc0bb6b07299deb27c37f8fc02c80d2c7f8d
Zoltan Kochan <z@kochan.io>
2021-11-22 22:59:05 +0200

    fix: don't add unnecessary trailing slash
    ref pnpm/pnpm#2933

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = function (registry) {
   if (typeof registry !== 'string') {
     throw new TypeError('`registry` should be a string')
   }
-  if (registry[registry.length - 1] === '/') return registry
+  if (
+    registry.endsWith('/') ||
+    registry.indexOf('/', registry.indexOf('//') + 2) != -1
+  ) return registry
   return `${registry}/`
 }
