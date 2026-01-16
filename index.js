805484aaddd2e711764fb43c7b3db0b4db48b883
Zoltan Kochan <z@kochan.io>
2026-01-16 17:05:02 +0100

    fix: remove redundant ports

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ module.exports = function (registry) {
   if (typeof registry !== 'string') {
     throw new TypeError('`registry` should be a string')
   }
+  try {
+    // Remove default ports (80 for HTTP, 443 for HTTPS) to ensure consistency
+    registry = new URL(registry).toString()
+  } catch {}
   if (
     registry.endsWith('/') ||
     registry.indexOf('/', registry.indexOf('//') + 2) != -1
